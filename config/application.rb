2d8f1ba50f23df05ed51c426cb98d8e77c23aa90
johan <johan.schantz@telia.com>
2017-03-23 18:05:26 +0100

    added stuff

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,12 @@ Bundler.require(*Rails.groups)
 module CooperApi
   class Application < Rails::Application
     Bundler.require(*Rails.groups)
-        config.middleware.insert_before 0, Rack::Cors do
-          allow do
-            origins '*'
-            resource '*', headers: :any, methods: [:get, :post, :put, :delete]
+            config.middleware.insert_before 0, Rack::Cors do
+              allow do
+                origins '*'
+                resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head],
+                 expose: %w(access-token expiry token-type uid client),
+                 max_age: 0
           end
         end
     config.api_only = true
